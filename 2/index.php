8b060f9196db75d7bff2f98ff402e1c4b651c9f1
markok1 <markogit123@gmail.com>
2024-06-24 21:46:25 +0200

    test

---
+++ 2/index.php
@@ -0,0 +1,382 @@
+<!-- <?php  if(isset($_GET['service'])) $serviceName = htmlspecialchars($_GET['service']); ?> -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<!-- begin Convert Experiences code--><script type="text/javascript" src="//cdn-4.convertexperiments.com/v1/js/10046288-10046734.js"></script><!-- end Convert Experiences code -->
+    <!-- Google Tag Manager -->
+    <script>
+      (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", "GTM-5RFSNLZ");
+    </script>
+    <!-- End Google Tag Manager -->
+    <meta charset="UTF-8" />
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="referrer" content="unsafe-url" />
+    <title>Voolt Get Started</title>
+    <link rel="icon" type="image/x-icon" href="../../img/favicon.png" />
+    <link href="../styled-components/main.css" rel="stylesheet" />
+    <link href="../styled-components/mediaQueries.css" rel="stylesheet" />
+    <link href="../styled-components/buttons.css" rel="stylesheet" />
+    <link href="../styled-components/LoadingIcon.css" rel="stylesheet" />
+    <link
+      href="../styled-components/WebsiteBuilderPreview.css"
+      rel="stylesheet"
+    />
+    <link href="../styled-components/InputCheckbox.css" rel="stylesheet" />
+    <link href="../styled-components/PageTransition.css" rel="stylesheet" />
+    <link
+      href="../styled-components/FlowLocationSelector.css"
+      rel="stylesheet"
+    />
+    <link href="../styled-components/InputWithAnswer.css" rel="stylesheet" />
+    <link href="../styled-components/websitePreview.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../style.css" />
+    <style>
+      .container {
+  display: flex;
+  align-items: center; /* Centers the image vertically */
+  justify-content: center; /* Centers the image horizontally */
+  height: 100vh; /* Full height of the viewport */
+}
+.responsive-img {
+  max-width: 400px;
+  width: 100%;
+  height: auto;
+}
+@media screen and (max-width: 1880px) {
+  .step2-phone {
+    max-width: 225px;
+  }
+}
+@media screen and (max-width: 768px) {
+  .step2-phone {
+    height: 100%;
+    position: absolute;
+    bottom: -78px;
+  }
+}
+@media screen and (max-width: 500px) {
+  .step2-phone {
+    height: 100%;
+    position: absolute;
+    bottom: -48px;
+    right: calc(50% - 135px);
+  }
+}
+
+/* Optional: Ensure that this styling applies only on desktop */
+@media (max-width: 768px) {
+  .container {
+    height: auto;
+  }
+}
+.hidden-div {
+  display: none;
+}
+</style>
+    </style>
+  </head>
+  <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-5RFSNLZ"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <div style="display: contents; width: 100%; height: 100%">
+      <main id="step-2">
+        <div class="info-panel website step2-bg">
+          <div
+            class="pageTransition svelte-lri320"
+            style="
+              z-index: 1;
+              width: 100%;
+              display: flex;
+              justify-content: center;
+              position: sticky;
+              top: 0;
+              padding-bottom: 50px;
+            "
+          >
+            <div>
+              
+              <!-- <div class="browser svelte-2b743j"> -->
+              <!-- <div class="browser-design svelte-2b743j">
+                <div class="svelte-2b743j"></div>
+                <div class="svelte-2b743j"></div>
+                <div class="svelte-2b743j"></div>
+              </div> -->
+              <div class="container step2-container">
+                <img src="../files/step2-phone.webp" class="responsive-img step2-phone">
+                <!-- Replace unnecessary image tags with a responsive image tag -->
+                <!-- <picture>
+                  <source
+                    media="(max-width: 989px)"
+                    srcset="../files/website-preview-mobile.png"
+                  />
+                  <source
+                    media="(min-width: 990px)"
+                    srcset="../files/website-preview-desktop.png"
+                  />
+                  <img
+                    src="../files/website-preview-desktop.png"
+                    alt="Voolt website preview"
+                  />
+                </picture> -->
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="pageTransition"
+          style="
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            z-index: 999;
+            background: #fff;
+          "
+        >
+          <div class="page-header">
+            <div class="logo-voolt">
+              <img src="../files/logo.svg" alt="Voolt Logo" />
+            </div>
+            <div
+              class="signin-button svelte-1cj47c6"
+              data-svelte-h="svelte-17udhf8"
+            >
+              <div>Have an account?</div>
+              <a href="https://app.voolt.com/login/" class="svelte-1cj47c6"
+                >Sign in</a
+              >
+            </div>
+          </div>
+          <div class="question-wrapper">
+            <div class="question-inner">
+              <div
+                class="pageTransition"
+                style="
+                  display: flex;
+                  justify-content: center;
+                  flex-direction: column;
+                  z-index: 999;
+                  background: #fff;
+                "
+              >
+                <div
+                  class="wrapper"
+                  style="height: auto; vertical-align: middle"
+                >
+                  <div class="content">
+                    <div class="header">
+                      <button
+                        id="back-button-website"
+                        class="back-button-website svelte-y1tow2"
+                      >
+                        <svg
+                          width="16"
+                          height="16"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M10 19L3.25 12L10 5"
+                            stroke="#565B61"
+                            stroke-width="1.4"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                          ></path>
+                        </svg>
+                        <span class="svelte-y1tow2">Back</span>
+                      </button>
+                      <h1 class="svelte-y1tow2">
+                        Add Your Business Details
+                        <!-- Let’s customize your <span style="color:#8dc63f;"><?php echo $serviceName; ?></span> website -->
+                      </h1>
+                      <p class="kicker">So we can prepare your targeted ads</p>
+                      <p class="svelte-y1tow2"></p>
+                    </div>
+                    <div class="user-interact svelte-y1tow2">
+                      <div class="input-title">1. Business name:</div>
+                      <label
+                        for="business-name-input-text"
+                        class="input-text svelte-1xnbu4k"
+                      >
+                        <input
+                          autocomplete="off"
+                          type="text"
+                          autocapitalize="off"
+                          class="input-text_input data-hj-allow svelte-1xnbu4k"
+                          name=""
+                          id="business-name-input-text"
+                          placeholder="Your business name"
+                          maxlength="60"
+                        />
+                      </label>
+                      <br />
+                      <div class="input-title">
+                        2. Business Location: (City or physical address for targeting)
+                      </div>
+                      <div class="autocomplete-address svelte-v9xh0">
+                        <label
+                          for="address-search-input"
+                          class="input-text svelte-1xnbu4k"
+                        >
+                          <input
+                            autocomplete="off"
+                            type="text"
+                            autocapitalize="off"
+                            class="input-text_input data-hj-allow svelte-1xnbu4k"
+                            name=""
+                            id="address-search-input"
+                            placeholder="Where is your business located?"
+                            maxlength="false"
+                          />
+                        </label>
+                        <div id="address-search-results"></div>
+                      </div>
+                      <br />
+                      <div class="input-title" >
+                        <div class="hidden-div">
+                        3- How many people are in your business?
+                      </div>
+                      <div class="hidden-div">
+                        <div class="overflow-wrapper svelte-1pq0p8h">
+                          <div class="overflow-track row svelte-1pq0p8h">
+                            <input
+                              class="input-checkbox_input svelte-1pq0p8h"
+                              type="radio"
+                              value="Team"
+                              name="team-size"
+                              id="team"
+                              checked
+                            />
+                            <label
+                              class="input-checkbox_label svelte-1pq0p8h"
+                              for="team"
+                            >
+                              <div class="radio-box svelte-1pq0p8h"></div>
+                              <div class="svelte-1pq0p8h">
+                                <div class="smaller-label svelte-1pq0p8h">
+                                  More than one
+                                </div>
+                                <div class="eg-label svelte-1pq0p8h">
+                                  e.g About Us
+                                </div>
+                              </div> </label
+                            ><input
+                              class="input-checkbox_input svelte-1pq0p8h"
+                              type="radio"
+                              name="team-size"
+                              value="Just myself"
+                              id="just-myself"
+                            />
+                            <label
+                              class="input-checkbox_label svelte-1pq0p8h"
+                              for="just-myself"
+                            >
+                              <div class="radio-box svelte-1pq0p8h"></div>
+                              <div class="svelte-1pq0p8h">
+                                <div class="smaller-label svelte-1pq0p8h">
+                                  Just me
+                                </div>
+                                <div class="eg-label svelte-1pq0p8h">
+                                  e.g About Me
+                                </div>
+                              </div>
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="footer svelte-y1tow2 step2-button">
+                    <div class="button svelte-y1tow2">
+                      <button
+                        id="create-website-button"
+                        class="button--primary button-with-loader"
+                      >
+                        Next
+                        <div class="loader-type1">
+                          <svg class="rotation" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 32 32" fill="none">
+                            <path
+                              d="M30 16C30 17.8385 29.6379 19.659 28.9343 21.3576C28.2308 23.0561 27.1995 24.5995 25.8995 25.8995C24.5995 27.1995 23.0561 28.2307 21.3576 28.9343C19.659 29.6379 17.8385 30 16 30C14.1615 30 12.341 29.6379 10.6424 28.9343C8.94387 28.2307 7.40052 27.1995 6.1005 25.8995C4.80048 24.5995 3.76925 23.0561 3.06569 21.3576C2.36212 19.659 2 17.8385 2 16C2 14.1615 2.36212 12.341 3.06569 10.6424C3.76926 8.94387 4.80049 7.40052 6.10051 6.1005C7.40053 4.80048 8.94388 3.76925 10.6424 3.06568C12.341 2.36212 14.1615 2 16 2C17.8385 2 19.659 2.36212 21.3576 3.06569C23.0561 3.76926 24.5995 4.80049 25.8995 6.10051C27.1995 7.40053 28.2308 8.94388 28.9343 10.6424C29.6379 12.341 30 14.1615 30 16L30 16Z"
+                              stroke="#EBEDEF"
+                              stroke-width="4"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                            <path
+                              d="M6.10051 6.1005C7.66275 4.53826 9.57255 3.36793 11.6738 2.68521C13.775 2.00248 16.0079 1.82675 18.1901 2.17236C20.3722 2.51798 22.4416 3.37514 24.229 4.67376C26.0164 5.97238 27.4711 7.67559 28.4741 9.64414C29.4771 11.6127 30 13.7907 30 16C30 18.2093 29.4771 20.3873 28.4741 22.3559C27.4711 24.3244 26.0164 26.0276 24.229 27.3262C22.4416 28.6249 20.3722 29.482 18.1901 29.8276"
+                              stroke="#058BEB"
+                              stroke-width="4"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                            />
+                          </svg>
+                          <p class="text-xs--regular">Loading...</p>
+                        </div>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <style data-svelte-h="svelte-tdbfzw">
+                  .input-loading {
+                    content: "";
+                    height: 48px;
+                    width: 100%;
+                    font-size: 16px;
+                    background: #e4e6e7;
+                    margin-bottom: 16px;
+                    border-radius: 8px;
+                  }
+
+                  .label-loading {
+                    content: "";
+                    width: 15%;
+                    height: 13px;
+                    background: #e4e6e7;
+                    margin-bottom: 10px;
+                    border-radius: 25px;
+                  }
+
+                  .input-title {
+                    font-size: 15px;
+                    font-weight: 500;
+                    margin-bottom: 10px;
+                    color: #2d3239;
+                    /* opacity:.7; */
+                  }
+
+                  .input-sub-title {
+                    font-size: 13px;
+                    font-weight: 500;
+                    margin-bottom: 10px;
+                    color: #2d3239;
+                    opacity: 0.7;
+                  }
+                </style>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+    <script src="../main.js?v=1"></script>
+  </body>
+</html>
